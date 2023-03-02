06d786ecf3106f92824002186cd6d0feb722279e
LiqunWu <lqwu@nreal.ai>
2023-03-02 15:02:15 +0800

    Initial Commit

---
+++ iOSMultiDisplay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSMultiDisplay
+//
+//  Created by Nreal on 2023/3/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
